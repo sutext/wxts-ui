d2762f474833e7299e4dec43f63be323cbfcffe9
supertext <supertext@icloud.com>
2018-10-23 00:52:23 +0800

    bug of config

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
 var config = require('wecp')
-config.theme = '#ffca50'
-config.grader.theme = { on: '/pages/index/icon_star_on.png', off: '/pages/index/icon_star_off.png' }
+config.env.theme = '#ffca50'
+config.env.grader.themes.theme = { on: '/pages/index/icon_star_on.png', off: '/pages/index/icon_star_off.png' }
 App({});
