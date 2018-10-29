f1d3f33bf45d260c73f2f806591318c0a382a4b7
supertext <supertext@icloud.com>
2018-10-29 17:19:21 +0800

    bug of button color

--- test/app.js
+++ test/app.js
@@ -1,7 +1,7 @@
 var wecp = require('wecp')
-wecp.env.theme = 'red'
-wecp.env.button.lineColor = '#ffca50'
-wecp.env.button.textColor = "#666"
-wecp.env.grader.type = 'gold'
-wecp.env.grader.themes.gold = { on: '/pages/index/icon_star_on.png', off: '/pages/index/icon_star_off.png' }
+wecp.conf.theme = 'red'
+wecp.conf.button.lineColor = '#ffca50'
+wecp.conf.button.textColor = "#666"
+wecp.conf.grader.type = 'gold'
+wecp.conf.grader.themes.gold = { on: '/pages/index/icon_star_on.png', off: '/pages/index/icon_star_off.png' }
 App({});
